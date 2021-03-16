cb008010dd311b1314f219fda6f21d4d19842b39
fyuneru0830 <36604859+fyuneru0830@users.noreply.github.com>
2021-03-17 00:49:34 +0900

    Update script.js

--- script/script.js
+++ script/script.js
@@ -56,7 +56,7 @@ const app = new Vue({
       this.id = idArray[idArray.length - 1];
     },
     getUpdate:function(){
-      setInterval(() => { this.getData() }, 5000)
+      setInterval(() => { this.getData() }, 3000)
     }
   }
    
