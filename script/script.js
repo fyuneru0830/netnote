bbfeb1f26ed58b1e4e3ed150fd186d7f314f18ca
fyuneru0830 <36604859+fyuneru0830@users.noreply.github.com>
2021-03-17 22:33:49 +0900

    Update script.js

--- script/script.js
+++ script/script.js
@@ -24,7 +24,7 @@ const app = new Vue({
   },
   created : function(){
     //update the data from the server
-    this.getUpdate();
+    this.autoUpdate();
   },
   mounted : function(){
     //get the note id from the URL
