f2062537cdccf5e0667ed333ec906cb5ced53d58
fyuneru0830 <36604859+fyuneru0830@users.noreply.github.com>
2021-03-17 22:18:32 +0900

    Update script.js

--- script/script.js
+++ script/script.js
@@ -39,7 +39,7 @@ const app = new Vue({
       axios.get('http://47.241.127.18/api/get.php?id='+ this.id)
       .then(response => this.serverSide = response.data)
       .catch(error => console.log(error));
-      for(let i = 0;i<100;i++){
+      for(let i = 0;i<10000;i++){
         console.log("serversideis "+ this.serverSide);
       }
     },
