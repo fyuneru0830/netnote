28f904af7ab5194c699936e0c3c517f0463b3cdf
fyuneru0830 <36604859+fyuneru0830@users.noreply.github.com>
2021-03-21 22:15:12 +0900

    big edit

--- script/script.js
+++ script/script.js
@@ -30,8 +30,6 @@ const app = new Vue({
   mounted : function(){
     //get the note id from the URL
       this.getId();
-    //set Title and Description
-      this.setTitleandDes();
     //get the data from the server
       this.firstGetData();
       
@@ -82,17 +80,6 @@ const app = new Vue({
       }else{
         this.diff = true;
       }
-    },
-    setTitleandDes:function(){
-      const title = this.getId
-      const description = ""
-      document.title = title
-      document.querySelector("meta[property='og:title']")
-      　.setAttribute('content', title)
-      document.querySelector("meta[name='description']")
-      　.setAttribute('content', description)
-      document.querySelector("meta[property='og:description']")
-      　.setAttribute('content', description)
     },
 
     //automatic get update
